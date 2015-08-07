f7fa5aea92ca9ce6c92abf43fae85230a98db8b4
8280yds <8280yds@163.com>
2015-08-07 17:13:46 +0800

    !更新配置资源

---
+++ modules/Code/Game/DB/DBVO/CellWarSceneDBVO.cs
@@ -0,0 +1,47 @@
+using Freamwork;
+using System.Xml;
+
+public class CellWarSceneDBVO : DBVO
+{
+	/// <summary>
+	/// 细胞阵营
+	/// </summary>
+	public string cellCamp;
+
+	/// <summary>
+	/// 细胞生命
+	/// </summary>
+	public string cellHP;
+
+	/// <summary>
+	/// 细胞x位置
+	/// </summary>
+	public string cellX;
+
+	/// <summary>
+	/// 细胞y位置
+	/// </summary>
+	public string cellY;
+
+	/// <summary>
+	/// 星级(时间S)
+	/// </summary>
+	public string star;
+
+	/// <summary>
+	/// AI计算时间间隔(ms)
+	/// </summary>
+	public int deleTime;
+
+	public override void xmlToVo(XmlNode node)
+	{
+		XmlElement xmlelement = (XmlElement)node;
+		id = int.Parse(xmlelement.GetAttribute("id"));
+		cellCamp = xmlelement.GetAttribute("cellCamp");
+		cellHP = xmlelement.GetAttribute("cellHP");
+		cellX = xmlelement.GetAttribute("cellX");
+		cellY = xmlelement.GetAttribute("cellY");
+		star = xmlelement.GetAttribute("star");
+		deleTime = int.Parse(xmlelement.GetAttribute("deleTime"));
+	}
+}
